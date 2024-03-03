00b9678ba565140e6ee4b43d1f8e00e3aa079deb
Adam Hjerpe <adam.hjerpe@ica.se>
2024-03-03 11:15:04 +0100

    Testing action on comment

--- chat_ops.py
+++ chat_ops.py
@@ -3,5 +3,6 @@ from ghapi.all import GhApi
 owner, repo = os.environ['GITHUB_REPOSITORY'].split('/')
 api = GhApi(owner=owner, repo=repo, token=os.environ['GITHUB_TOKEN'])
 PR_NUMBER = os.environ['PR_NUMBER']
+print(PR_NUMBER)
 comments = api.pulls.get_review_comment(owner=owner, repo=repo, comment_id=PR_NUMBER)
 print(comments)
